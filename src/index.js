ac170fe3f214f8bae93638a595274d3ddd9313e7
slabak <slabakxaker@gmail.com>
2022-06-16 22:38:34 +0300

    env process

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/world-future/">
+        <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
           <ToastContainer
             position="top-right"
             autoClose={3000}
