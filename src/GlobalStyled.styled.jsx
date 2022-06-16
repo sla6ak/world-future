49f9ce3018b8ae3872efac8d02af78048521ded1
slabak <slabakxaker@gmail.com>
2022-06-16 19:10:05 +0300

    root

--- src/GlobalStyled.styled.jsx
+++ src/GlobalStyled.styled.jsx
@@ -6,8 +6,8 @@ export const GlobalStyle = () => {
       styles={css`
         body {
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
+            Cantarell, 'Helvetica Neue', sans-serif;
           color: #212121;
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
@@ -29,13 +29,7 @@ export const GlobalStyle = () => {
           box-sizing: border-box;
         }
         code {
-          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-            monospace;
-        }
-        .root {
-          display: flex;
-          flex-direction: column;
-          min-height: 100vh;
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
         }
         a {
           color: rgb(48, 121, 255);
