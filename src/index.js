4c73b60030ab3acbb93a0a79c945932648e27e75
slabak <slabakxaker@gmail.com>
2022-06-16 19:50:12 +0300

    https://world-future.vercel.app/

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/world-future/">
+        <BrowserRouter basename="/world-future.vercel.app/">
           <ThemeProvider theme={theme}>
             <ToastContainer
               position="top-right"
