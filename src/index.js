65d90bf1efae195a389f0af84b4d61c5a02c8968
slabak <slabakxaker@gmail.com>
2022-06-16 19:58:18 +0300

    C:/Program Files/Git/world-future/

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/world-future.vercel.app/">
+        <BrowserRouter basename="/world-future/">
           <ThemeProvider theme={theme}>
             <ToastContainer
               position="top-right"
