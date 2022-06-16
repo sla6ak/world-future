34f53639efddb5ee6908645007e46f99ee8191f8
slabak <slabakxaker@gmail.com>
2022-06-16 22:00:59 +0300

    deleted json

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
-import { theme } from './constants/theme';
-import { ThemeProvider } from '@emotion/react';
 import { ToastContainer, Zoom } from 'react-toastify';
 import { GlobalStyle } from './GlobalStyled.styled';
 
@@ -17,23 +15,21 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/world-future/">
-          <ThemeProvider theme={theme}>
-            <ToastContainer
-              position="top-right"
-              autoClose={3000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="dark"
-              transition={Zoom}
-            />
-            <GlobalStyle />
-            <App />
-          </ThemeProvider>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition={Zoom}
+          />
+          <GlobalStyle />
+          <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
