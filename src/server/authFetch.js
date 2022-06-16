9fab8924f3216e613a11511e51fff74949a2ae92
slabak <slabakxaker@gmail.com>
2022-06-16 18:42:15 +0300

    link to hiroku

--- src/server/authFetch.js
+++ src/server/authFetch.js
@@ -1,44 +1,48 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const PORT = process.env.PORT || "https://world-future.herokuapp.com";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const PORT = 'https://world-future.herokuapp.com';
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
-    reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${PORT}/auth/`,
-        prepareHeaders: (headers, { getState }) => {
-            const token = getState().token;
-            if (token) {
-                headers.set("authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${PORT}/auth/`,
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+
+  endpoints: builder => ({
+    tagTypes: ['curentUser'],
+    getIsActivUser: builder.query({
+      query: () => ({
+        url: `/current`,
+        method: 'GET',
+      }),
+    }),
+    registrationUser: builder.mutation({
+      query: newUser => ({
+        url: '/signup',
+        method: 'POST',
+        body: newUser,
+      }),
     }),
 
-    endpoints: (builder) => ({
-        tagTypes: ["curentUser"],
-        getIsActivUser: builder.query({
-            query: () => ({
-                url: `/current`,
-                method: "GET",
-            }),
-        }),
-        registrationUser: builder.mutation({
-            query: (newUser) => ({
-                url: "/signup",
-                method: "POST",
-                body: newUser,
-            }),
-        }),
-
-        loginUser: builder.mutation({
-            query: (userData) => ({
-                url: `/login`,
-                method: "POST",
-                body: userData,
-            }),
-        }),
+    loginUser: builder.mutation({
+      query: userData => ({
+        url: `/login`,
+        method: 'POST',
+        body: userData,
+      }),
     }),
+  }),
 });
 
-export const { useGetIsActivUserQuery, useRegistrationUserMutation, useLoginUserMutation } = authApi;
+export const {
+  useGetIsActivUserQuery,
+  useRegistrationUserMutation,
+  useLoginUserMutation,
+} = authApi;
