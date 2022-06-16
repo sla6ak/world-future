95e7d41168c6199fca162da1ff77b35662bbb320
slabak <slabakxaker@gmail.com>
2022-06-16 19:34:05 +0300

    base rout

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/world-future/">
           <ThemeProvider theme={theme}>
             <ToastContainer
               position="top-right"
